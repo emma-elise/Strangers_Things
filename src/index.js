76222a337a3ef0f3363cc617c547450905bbbdd3
John <jvt703@gmail.com>
2021-08-18 11:37:15 -0400

    Added some standard react imports and start/build scripts

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () =>{
+
+    return <h1> Test html </h1>
+
+
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app'),
+);
