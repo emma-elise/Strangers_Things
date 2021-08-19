2696853f30dded254e8be1ea1044cbb989759b01
John <jvt703@gmail.com>
2021-08-19 19:08:13 -0400

    added some basic state we may need

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () =>{
+
+    const [postList, setPostList] = useState([])
+    const [LocalToken, setLocalToken] = useState([])
+    useEffect( ()=>{
+    //fetch posts to setPostList https://strangers-things.herokuapp.com/api/2105-VPI-RM-WEB-PT/posts
+    //fetch messages to setMessageList? https://strangers-things.herokuapp.com/api/2105-VPI-RM-WEB-PT/users/me
+    //put the token to local storage? (maybe not here) and setLocalToken based off the token in local storage
+    },[]
+    )
 
     return <h1> Test html </h1>
 
