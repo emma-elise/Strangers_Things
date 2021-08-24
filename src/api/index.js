336d7a416bac05389b022f5c1bc09d160a1b3862
emma-elise <loirratse@gmail.com>
2021-08-24 13:58:28 -0400

    began fetchRegisterUser

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,19 @@
 import Post from "../components/Post";
 
-export const BASE_URL= "https://strangers-things.herokuapp.com/api/2105-VPI-RM-WEB-PT"
+const BASE_URL =
+  "https://strangers-things.herokuapp.com/api/2105-VPI-RM-WEB-PT";
 
-const loginToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTIxMDg1ZDZjYzIzNDAwMTcxN2ExYzAiLCJ1c2VybmFtZSI6InN1cGVybWFuc0JvZHlnYXVyZHMiLCJpYXQiOjE2Mjk1NTQ3ODF9.TGH8VtdQ1VrdlW1HMjhKpBqJr2BL_y9oPkLrAitqP2o'
+const loginToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTIxMDg1ZDZjYzIzNDAwMTcxN2ExYzAiLCJ1c2VybmFtZSI6InN1cGVybWFuc0JvZHlnYXVyZHMiLCJpYXQiOjE2Mjk1NTQ3ODF9.TGH8VtdQ1VrdlW1HMjhKpBqJr2BL_y9oPkLrAitqP2o";
 
-export async function checkLogin() {
- if (localStorage.getItem('loginToken')) {
-    return JSON.parse(localStorage.getItem('loginToken'));
+async function checkLogin() {
+  if (localStorage.getItem("loginToken")) {
+    return JSON.parse(localStorage.getItem("loginToken"));
   }
-
 }
 
-
-export async function fetchPosts() {
-
-  const url = `${ BASE_URL }/posts`;
+async function fetchPosts() {
+  const url = `${BASE_URL}/posts`;
 
   try {
     const response = await fetch(url);
@@ -26,13 +25,14 @@ export async function fetchPosts() {
   }
 }
 
-export async function fetchUserData() {
-   
-  const url = `${ BASE_URL }/users/me`;
-  const headers = { headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${ loginToken }` 
-  }}
+async function fetchUserData() {
+  const url = `${BASE_URL}/users/me`;
+  const headers = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${loginToken}`,
+    },
+  };
   try {
     const response = await fetch(url, headers);
     const obj = await response.json();
@@ -42,3 +42,23 @@ export async function fetchUserData() {
   }
 }
 
+const fetchRegisterUser = async () => {
+  const url = `${BASE_URL}/users/register`;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const body = JSON.stringify({
+    user: {
+      username: "",
+      password: "",
+    },
+  });
+  try {
+    const registerUser = await (await await fetch(url, headers, body)).json();
+    return registerUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { BASE_URL, checkLogin, fetchPosts, fetchUserData, fetchRegisterUser };
