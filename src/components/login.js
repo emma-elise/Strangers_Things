283c2413b9450ae4bdb090d9f11be124d7b02d82
emma-elise <loirratse@gmail.com>
2021-08-26 12:15:30 -0400

    set register/login token to localStorage

--- src/components/login.js
+++ src/components/login.js
@@ -3,14 +3,15 @@ import React, { Component, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { BASE_URL } from "../api";
 
-// TODO: Work on log in (only register set atm) and log out
+// TODO: Work on displaying if user is logged on; add logout feature
 
 const Login = () => {
   const [submitForm, setSubmitForm] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const userLoggedIn = false;
-  const authenticateUser = (event) => {
+  let loginStatus = false;
+
+  const registerUser = (event) => {
     event.preventDefault();
     console.log("form submitted");
     setSubmitForm(true);
@@ -22,20 +23,79 @@ const Login = () => {
       .post(`${BASE_URL}/users/register`, body, headers)
       .then((response) => {
         console.log(response);
+        const token = response.data.data.token;
+        localStorage.setItem("token", token);
+        loginStatus = !loginStatus;
       })
       .catch((error) => {
         console.log(error.response.data);
       });
   };
 
+  const loginUser = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+    setSubmitForm(true);
+    const body = JSON.stringify({
+      user: { username: username, password: password },
+    });
+    const headers = { headers: { "Content-Type": "application/json" } };
+    return axios
+      .post(`${BASE_URL}/users/login`, body, headers)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.data.token;
+        localStorage.setItem("token", token);
+        loginStatus = !loginStatus;
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  const logoutUser = () => {
+    if (localStorage.getItem("token") === null) return;
+    localStorage.removeItem("token");
+    loginStatus = !loginStatus;
+    location.reload();
+    console.log("logout clicked");
+  };
+
   return (
     <section className="login">
-      {!userLoggedIn && <h1>Login</h1>}
-      <form onSubmit={authenticateUser}>
+      {/* {!userLoggedIn && <h1>Login</h1>} */}
+      {/* {loginStatus && <h1>User Is Logged In</h1>} */}
+      {!loginStatus && localStorage.getItem("token") !== null && (
+        <h1>User Is Logged In</h1>
+      )}
+      <h3>Register</h3>
+      <form onSubmit={registerUser}>
         <div>
           <label>Username: </label>
           <input
             type="text"
+            required="required"
+            value={username}
+            onInput={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="text"
+            value={password}
+            onInput={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <h3>Login</h3>
+      <form onSubmit={loginUser}>
+        <div>
+          <label>Username: </label>
+          <input
+            type="text"
+            required="required"
             value={username}
             onInput={(event) => setUsername(event.target.value)}
           />
@@ -49,8 +109,11 @@ const Login = () => {
           />
         </div>
         <button type="submit">Login</button>
-        <button type="submit">Sign Up</button>
       </form>
+      <h3>Logout</h3>
+      <button type="submit" onClick={logoutUser}>
+        Logout
+      </button>
     </section>
   );
 };
